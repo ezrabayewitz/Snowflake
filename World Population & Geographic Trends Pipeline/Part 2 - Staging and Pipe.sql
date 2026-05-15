c27125544b80bc0a69f73997ea2da1763f0d3eba
Ezra Bayewitz <ejb8301@gmail.com>
2026-05-15 10:31:07 -0500

    Create Part 2 - Staging and Pipe.sql

---
+++ World Population & Geographic Trends Pipeline/Part 2 - Staging and Pipe.sql
@@ -0,0 +1,45 @@
+USE DATABASE WORLD_POPULATION;
+USE SCHEMA RAW;
+USE WAREHOUSE WORLD_WH;
+
+CREATE OR REPLACE FILE FORMAT world_bank_csv
+    TYPE = 'CSV'
+    FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+    SKIP_HEADER = 1
+    NULL_IF = ('NULL', 'null', '', 'NA', '..')
+    EMPTY_FIELD_AS_NULL = TRUE
+    TRIM_SPACE = TRUE
+
+-- Internal stage
+CREATE OR REPLACE STAGE world_stage
+    FILE_FORMAT = (FORMAT_NAME = world_bank_csv)
+    COMMENT = 'Internal stage for World Bank CSV uploads';
+
+-- Snowpipe for population data
+CREATE OR REPLACE PIPE pop_pipe
+    AUTO_INGEST = FALSE
+    AS
+    COPY INTO RAW.POPULATION_RAW
+    FROM @world_stage/population/
+    FILE_FORMAT = (FORMAT_NAME = world_bank_csv)
+    ON_ERROR = 'CONTINUE';
+
+-- Snowpipe for GDP/indicators data
+CREATE OR REPLACE PIPE indicators_pipe
+  AUTO_INGEST = FALSE
+  AS
+  COPY INTO RAW.INDICATORS_RAW
+  FROM @world_stage/indicators/
+  FILE_FORMAT = (FORMAT_NAME = world_bank_csv)
+  ON_ERROR = 'CONTINUE';
+
+-- Snowpipe for country metadata
+CREATE OR REPLACE PIPE countries_pipe
+  AUTO_INGEST = FALSE
+  AS
+  COPY INTO RAW.COUNTRIES_RAW
+  FROM @world_stage/countries/
+  FILE_FORMAT = (FORMAT_NAME = world_bank_csv)
+  ON_ERROR = 'CONTINUE';
+
+    
